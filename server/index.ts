1e112e6af1dd3aaa68a63d8479a803cf92d6bec3
ryann254 <keddelyronjoz@gmail.com>
2023-01-26 12:00:43 +0300

    fix: add dynamic port loading

--- server/index.ts
+++ server/index.ts
@@ -17,12 +17,12 @@ app.get('/', async (req, res) => {
   res.send('Hola de DALL-E, Home!');
 });
 
+const PORT = process.env.PORT || 8080;
+
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () =>
-      console.log('Server running on http://localhost:8080')
-    );
+    app.listen(PORT, () => console.log(`Server running on ${PORT}`));
   } catch (error) {
     console.log(error);
   }
